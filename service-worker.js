ead3e486eeb3bed024568d4a21ef5b76b17f427f
zapdonecz <109079202+zapdonecz@users.noreply.github.com>
2025-03-10 11:23:10 +0100

    Update 1.0.1. service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,48 @@
-const CACHE_NAME = 'bj-pomocnik-cache-v4';
+const CACHE_NAME = 'ip-hlidac-cache-v1';
 const urlsToCache = [
-  "index.html",
-  "css/style.css",
-  "js/app.js",
-  "js/settings.js",
-  "js/help.js",
-  "manifest.json"
+  './',
+  './index.html',
+  './style.css',      // Uprav, pokud máš jiný název souboru s logikou
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
+// Instalace: otevře cache a uloží vyjmenované soubory
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache otevřená');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Posluchač zpráv pro aktualizaci
+self.addEventListener('message', event => {
+  if (event.data && event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+// Aktivace: odstraní staré cache verze
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Odstraňuji starou cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Přesměrování fetch requestů: nejprve se pokusí najít odpověď v cache, pokud nenajde, stáhne ze sítě
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
