801f19da6ad5a121c43e2b93cfb0e00b9dff3e09
zapdonecz <109079202+zapdonecz@users.noreply.github.com>
2025-03-10 13:38:07 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,12 @@ const CACHE_NAME = 'ip-hlidac-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './style.css',      // Uprav, pokud máš jiný název souboru s logikou
+  './style.css',      // Uprav podle názvu tvého CSS souboru
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
-// Instalace: otevře cache a uloží vyjmenované soubory
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -19,14 +18,12 @@ self.addEventListener('install', event => {
   );
 });
 
-// Posluchač zpráv pro aktualizaci
 self.addEventListener('message', event => {
   if (event.data && event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
 });
 
-// Aktivace: odstraní staré cache verze
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames =>
@@ -42,7 +39,6 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Přesměrování fetch requestů: nejprve se pokusí najít odpověď v cache, pokud nenajde, stáhne ze sítě
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
